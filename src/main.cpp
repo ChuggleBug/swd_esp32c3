9991f2653ad511cca25d76061a20d10bfbe98b09
Jacob <jacobgutierrez752@gmail.com>
2025-04-03 23:12:39 -0700

    Implement basic memory write example

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,67 @@
-// Define the pin for the onboard LED
+
 #include <Arduino.h>
 
-#include "libswd/libswd.h"
+#include <libswd/libswd.h>
+#include <libswd/logger.h>
 #include "esp32mini_driver.h"
 
 // Note for tm4c123gh6pm
 //  - SWDIO <--> TMS
 //  - SWCLK <--> TCK
+// device is AHB-AP MEM-AP
+// by default, DRW transactions are 8 bits. check CSW register
+// by default, TAR auto are off for DRW transactions, check CSW
+
 // tm4c123gh6pm is set to JTAG by default
 // needs to be converted
-#define JTAG_DEFAULT
+
+using namespace swd;
+
+// Simple class for logging
+class Esp32Logger : public Logger {
+
+  public:
+
+    void begin(uint32_t baud) {
+        Serial.begin(baud);
+    }
+
+  protected:
+
+    virtual void write(const char *str) override {
+        Serial.print(str);
+    }
+
+    virtual void flush() override {
+        Serial.flush();
+    }
+};
 
 static Esp32Driver driver;
-static swd::SWDHost host = swd::SWDHost(&driver);
+static SWDHost host = SWDHost(&driver);
 
-void get_idcode();
-
-uint8_t calculateParity(uint32_t data) {
-    uint8_t parity = 0;
-    while (data) {
-        parity ^= (data & 1);  // XOR each bit
-        data >>= 1;
-    }
-    return parity;  // Returns 1 if odd number of 1s, otherwise 0
-}
+static Esp32Logger logger;
 
 void setup() {
-    Serial.begin(9600);
-    delay(1000);
+    logger.begin(9600);
+    Logger::setLogger(&logger);
+    Logger::setLogLevel(LogLevel::DEBUG);
+    
+    // Mostly the host not writing to serial 
+    // output too early 
+    delay(2000);
 
-    Serial.println("Host Begin");
-
-
-    pinMode(LED, OUTPUT);
-    digitalWrite(LED, HIGH);
-    delay(1000);
-    digitalWrite(LED, LOW);
-    delay(1000);
-
-    #ifdef JTAG_DEFAULT
-        Serial.print("Send init sequence\n");
-        host.init();   
-    #endif
+    host.reset();
 }
 
 void loop() {
-    // For now, values are hard coded
+    uint32_t target = 0x20000200;
+    uint32_t data = 0x80;
 
-    // Read IDCode
-    // Packet:
-    //  - Start: 1
-    //  - APnDP: 0
-    //  - RnW: 1
-    //  - A[2:3]: 00
-    //  - Parity: 1
-    //  - Stop: 0
-    //  - Park: 1
-    uint8_t packet = 0b10100101;
+    // Currently the target device listens for bit 7 @ 0x20000200
+    host.writePort(AP::TAR, target);
+    host.writePort(AP::DRW, data);
 
-    host.sendPacket(packet);
-    host.turnaround(1);
-    uint32_t ack = host.readACK();
-    // Reads have no turnaround
-    uint32_t rdata = host.readBits(32);
-    uint32_t parity = host.readBits(1);
-    host.turnaround(1);
-
-    Serial.printf("Packet send: 0x%x\n", packet);
-    Serial.printf("ACK read: 0x%x\n", ack);
-    if (ack == swd::ACK::OK) {
-        Serial.printf("ACK was OK\n");
-    }
-    Serial.printf("rdata: 0x%x, parity: 0x%x\n", rdata, parity);
-
-    // Spin since test is over
-    while (1) {
-        digitalWrite(LED, HIGH);
-        delay(1000);
-        digitalWrite(LED, LOW);
-        delay(1000);
-    }
+    // Only run this test code once
+    while (1);
 }
