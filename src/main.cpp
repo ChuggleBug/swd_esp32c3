69c1895a22cfcaf732e55d86e89cbfa23181e070
Jacob <jacobgutierrez752@gmail.com>
2025-04-04 22:29:40 -0700

    Add simple read/write functions

--- src/main.cpp
+++ src/main.cpp
@@ -42,10 +42,14 @@ static SWDHost host = SWDHost(&driver);
 
 static Esp32Logger logger;
 
+
+bool write_byte(uint32_t addr, uint8_t data);
+Optional<uint8_t> read_byte(uint32_t addr);
+
 void setup() {
     logger.begin(9600);
     Logger::setLogger(&logger);
-    Logger::setLogLevel(LogLevel::DEBUG);
+    Logger::setLogLevel(LogLevel::INFO);
     
     // Mostly the host not writing to serial 
     // output too early 
@@ -58,10 +62,27 @@ void loop() {
     uint32_t target = 0x20000200;
     uint32_t data = 0x80;
 
-    // Currently the target device listens for bit 7 @ 0x20000200
-    host.writePort(AP::TAR, target);
-    host.writePort(AP::DRW, data);
+    write_byte(target, data);
+    auto byte = read_byte(target);
+    
+    if ( byte.hasValue() ) {
+        Serial.printf("Data Read: 0x%x\n\r", byte.getValue());
+    } else {
+        Serial.println("No data read");
+    }
 
-    // Only run this test code once
     while (1);
+}
+
+
+bool write_byte(uint32_t addr, uint8_t data) {
+    return host.writePort(AP::TAR, addr) && host.writePort(AP::DRW, (uint32_t) data);
+}
+
+Optional<uint8_t> read_byte(uint32_t addr) {
+    auto data = Optional<uint32_t>::none();
+    if (host.writePort(AP::TAR, addr) && ( data = host.readPort(AP::DRW)).hasValue() ) {
+        return Optional<uint8_t>::of((uint8_t) data.getValue());
+    }
+    return Optional<uint8_t>::none();
 }
